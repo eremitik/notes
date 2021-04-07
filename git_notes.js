31fa0b74ff012aea1a288a67e05025fbef2ec69b
eremitik <39305876+eremitik@users.noreply.github.com>
2021-04-07 12:04:02 +0900

    Create git_notes.js

---
+++ git_notes.js
@@ -0,0 +1,20 @@
+git status --- see which branch you're in
+git branch-a --- see all available branches
+git checkout xxx --- switch to xxx branch
+git add . --- stage changes to upload
+git push origin xxx --- pushes to online git repo
+
+[to sync with master]
+git checkout master
+git pull
+git checkout feature/xxx-branch
+git merge master
+
+[to push xxx-branch to master]
+git checkout master
+git merge feature/xxx-branch
+git push origin master
+
+[git cloning repo]
+git clone -b xxx https://
+(-b is branch call, xxx is branch name
