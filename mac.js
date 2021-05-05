f5a7260c8cb5b472607d87b14c7e28c826af069a
eremitik <39305876+eremitik@users.noreply.github.com>
2021-05-05 16:24:52 +0900

    Update mac.js

--- mac.js
+++ mac.js
@@ -1,2 +1,3 @@
 ^right or left --- switch btwn desktops
 option+command+u,i,j,k --- for Tiles app, push window into a corner on desktop
+command+w --- closes only the current window, not whole application
