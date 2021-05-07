a82e008df8db737e0717fba12a681fcc0adfa90d
eremitik <39305876+eremitik@users.noreply.github.com>
2021-05-07 15:21:26 +0900

    Update mac.js

--- mac.js
+++ mac.js
@@ -5,3 +5,4 @@ command+w --- closes only the current window, not whole application
 
 [Links]
 For keyboard shortcut to open terminal: https://clay-atlas.com/us/blog/2020/12/14/mac-os-en-open-terminal-by-shortcut-key/
+For Ubuntu mono font: http://jr0cket.co.uk/2013/08/developing-on-macosx-more-human-with.html.html
