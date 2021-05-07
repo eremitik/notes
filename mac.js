4f121470f45aeaa65861426f767766f26299d543
eremitik <39305876+eremitik@users.noreply.github.com>
2021-05-07 10:21:02 +0900

    Update mac.js

--- mac.js
+++ mac.js
@@ -1,3 +1,7 @@
 ^right or left --- switch btwn desktops
 option+command+u,i,j,k --- for Tiles app, push window into a corner on desktop
 command+w --- closes only the current window, not whole application
+
+
+[Links]
+For keyboard shortcut to open terminal: https://clay-atlas.com/us/blog/2020/12/14/mac-os-en-open-terminal-by-shortcut-key/
