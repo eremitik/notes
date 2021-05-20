18a0d9ca31f2d8a51cd2294a9e5a2ea7d33a1b5e
eremitik <39305876+eremitik@users.noreply.github.com>
2021-05-21 00:35:15 +0900

    Update vsc.js

--- vsc.js
+++ vsc.js
@@ -1,5 +1,7 @@
 //shortcuts
 cmd + \ --- split window
 cmd + 1, cmd + 2, etc. --- jump between split windows
-cmd + shift + e --- jump to Explorer (side menu)
+cmd + E --- jump to Explorer (side menu)
 cmd + b --- hide Explorer
+cmd + P --- shell command
+cmd + ` --- terminal
