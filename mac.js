e185733931726df89ad278ddd7b07d4e72a3818b
eremitik <39305876+eremitik@users.noreply.github.com>
2021-06-02 14:50:51 +0900

    Update mac.js

--- mac.js
+++ mac.js
@@ -3,6 +3,7 @@ option+command+u,i,j,k --- for Tiles app, push window into a corner on desktop
 command+w --- closes only the current window, not whole application
 command+`--- scroll through open windows in an app (i.e. go through all the terminal windows)
 command+> --- toggle show/hide hidden files+folders
+command+delete --- send file to trash
 
 [Links]
 For keyboard shortcut to open terminal: https://clay-atlas.com/us/blog/2020/12/14/mac-os-en-open-terminal-by-shortcut-key/
