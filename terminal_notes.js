0adba21654d736a74779de6fd0f8d7e9d40d7389
eremitik <39305876+eremitik@users.noreply.github.com>
2021-06-14 09:20:11 +0900

    Update terminal_notes.js

--- terminal_notes.js
+++ terminal_notes.js
@@ -4,5 +4,6 @@ mkdir xxx --- create folder named xxx
 renaming file --- $mv oldfile.js newfile.js
 open image --- $inkview xxx.svg
 copy file --- $cp App.js CopyApp.js
+delete .ds_store file --- $ find . -name '.DS_Store' -type f -ls -delete
 
 */
