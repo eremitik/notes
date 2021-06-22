9c7806e0330c6218f84cc67c5f8f97fefc07f7cf
eremitik <39305876+eremitik@users.noreply.github.com>
2021-06-22 21:41:50 +0900

    Create zoom.js

---
+++ zoom.js
@@ -0,0 +1,4 @@
+// keyboard shortcuts while in app
+cmd + shift + a === mute/unmute toggle
+spacebar === temporarily unmute (hold down spacebar while talking)
+cmd + shift + v === video on/off toggle 
